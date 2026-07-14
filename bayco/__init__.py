6b9a3aeca9270dec33983827bda95b54e2cd844b
gyiasoumi <yiasgeorgia@outlook.com>
2026-07-14 20:47:20 +0100

    updated function names
    updated some function names and added a combine networks for the TF targets

--- bayco/__init__.py
+++ bayco/__init__.py
@@ -1,8 +1,9 @@
 from .functions import (
     df_to_prepareddf,
-    df_to_BFs,
-    df_to_BFs_all_results,
-    combine_networks,
+    df_to_BFs_allgenes,
+    df_to_BFs_allgenes_all_results,
+    combine_networks_allgenes,
     make_tf_target_pairs,
     df_to_BFs_tf_vs_targets,
+    combine_tf_target_networks,
 )
